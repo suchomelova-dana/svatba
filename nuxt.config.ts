8aa65754c5b347bcadad3aee6fd3755e9cbc022c
SamuelSvalich <samuel.svalich@newsmatics.com>
2026-01-03 12:01:27 +0100

    set repo name

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,10 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   ssr: true, // keep SSR on; we'll pre-render to static via generate
   app: {
-    baseURL: "/", // <-- important
-    // Optional but recommended for GitHub Pages:
-    // GitHub Pages + Jekyll can ignore folders starting with "_"
-    buildAssetsDir: "/assets/",
+    baseURL: '/svatba/',
+    buildAssetsDir: '/assets/'
   },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
