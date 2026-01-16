3a2467db93560c2b10f8322a100cbf038bb13130
SamuelSvalich <samuel.svalich@newsmatics.com>
2026-01-16 17:19:28 +0100

    create base design

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,14 +4,23 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   ssr: true, // keep SSR on; we'll pre-render to static via generate
   app: {
-    baseURL: '/',
-    buildAssetsDir: '/assets/'
+    baseURL: "/svatba/",
+    buildAssetsDir: "/assets/",
   },
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxt/ui"],
+  modules: ["@nuxt/eslint", "@nuxt/ui", "@nuxtjs/google-fonts"],
   css: ["./app/assets/css/main.css"],
   vite: {
     plugins: [tailwindcss()],
+  },
+  googleFonts: {
+    families: {
+      "Cormorant Garamond": [300,400, 500, 600, 700],
+      // Roboto: true, // shortcut for all weights
+    },
+    display: "swap",
+    preconnect: true,
+    preload: true,
   },
 });
